ef5f6d78eac080945fdb7dbb10d139ae5da63fee
Ricardo Persaud <ricardo39985@gmail.com>
2020-01-06 21:33:33 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,18 @@
+class Doctor
+  attr_reader :name
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  def appointments
+    Appointment.all.select { |appointment|appointment.doctor == self }
+  end
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end
+  def patients
+    Appointment.all.select{|appointment|appointment.doctor == self}.map { |appointment|appointment.patient }
+  end
+
+end
