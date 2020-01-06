46371f3d387b3be4c8658b95749f7fa0d370a6cb
Ricardo Persaud <ricardo39985@gmail.com>
2020-01-06 21:44:02 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -15,7 +15,7 @@ class Doctor
     Appointment.new(date, patient, self)
   end
   def patients
-    Appointment.all.select{|appointment|appointment.doctor == self}.map { |appointment|appointment.patient }
+    Appointment.all.select{|appointment|appointment.doctor == self}.map { |appointment|appointment }
   end
 
 end
