9f6b5ca952d92643a7eeda7b00bfd4f3d45b7d47
Ricardo Persaud <ricardo39985@gmail.com>
2020-01-06 22:05:05 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -12,7 +12,7 @@ class Doctor
     Appointment.all.select { |appointment|appointment.doctor == self }
   end
   def new_appointment(patient, date)
-    binding.pry
+    # binding.pry
     Appointment.new(date, patient, self)
   end
   def patients
