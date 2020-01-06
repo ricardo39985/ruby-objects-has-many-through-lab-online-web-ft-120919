02d876ac5f32f746210737c63a68f2ba0b6e81ec
Ricardo Persaud <ricardo39985@gmail.com>
2020-01-06 21:36:26 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,14 @@
+class Appointment
+  attr_reader :patient, :doctor, :date
+  @@all = []
+  def initialize(date, patient, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+
+end
