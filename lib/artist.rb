24e02ada7e474a7a6b0a7f078b9492b011a51757
Ricardo Persaud <ricardo39985@gmail.com>
2020-01-06 21:15:54 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,19 +3,19 @@ class Artist
   @@all = []
   def initialize(name)
     @name = name
-    @@all << self    
+    @@all << self
   end
 
   def self.all
     @@all
   end
   def songs
-    Songs.all.select{|song|song.artist == self}    
+    Songs.all.select{|song|song.artist == self}
   end
   def new_song(song_name, song_genre)
-    Song.new(song_name, song_genre, self)    
+    Song.new(song_name, song_genre, self)
   end
   def genres
-    Song.all.select { |song|.song.artist == self}.map { |song|song.genre  }    
+    Song.all.select { |song|.song.artist == self}.map { |song|song.genre  }
   end
 end
