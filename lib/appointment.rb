941b587fb7e7d1c41ba533c083d02d8e9455bd1e
Ricardo Persaud <ricardo39985@gmail.com>
2020-01-06 21:55:24 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -2,9 +2,9 @@ require "pry"
 class Appointment
   attr_reader :date, :patient, :doctor
   @@all = []
-  def initialize(date, patient, doctor)
-    @date = patient
-    @patient = date
+  def initialize(patient, date, doctor)
+    @date = date
+    @patient = patient
     @doctor = doctor
     @@all << self
     # binding.pry
